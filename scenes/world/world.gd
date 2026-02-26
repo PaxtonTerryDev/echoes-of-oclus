953aa91e85eedfe989846b7d64cc648b8a3b3330
Paxton Terry <117777648+PaxtonTerryDev@users.noreply.github.com>
2026-02-25 18:14:51 -0700

    defines test scene for world rendering

--- scenes/world/world.gd
+++ scenes/world/world.gd
@@ -1,7 +1,14 @@
 class_name World extends Node2D
 
+var log = Log.new(["World"])
+
 signal world_initialized()
+
+@export var grid: WorldGrid = WorldGrid.new()
 
 func initialize() -> void:
 	world_initialized.emit()
 	return
+
+var small_grid = GridLayer.create(8)
+var big_grid = GridLayer.create(64)
